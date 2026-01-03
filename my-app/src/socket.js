eb423cf3d5f00aa3858df4705d3f9206829e1875
jacky <jackyyadav7565@gmail.com>
2026-01-03 11:40:27 -0800

    dynamic url

--- my-app/src/socket.js
+++ my-app/src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
- let liveServer = "https://realtime-chat-8mc17k0ok-jacky-yadavs-projects.vercel.app/"
-
+//  let liveServer = "https://realtime-chat-8mc17k0ok-jacky-yadavs-projects.vercel.app/"
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL.replace("/api", "");
 // let localServer = "http://localhost:5000"
-export const socket = io(liveServer, {
+export const socket = io(SOCKET_URL, {
   withCredentials: true,
   autoConnect: true
 });

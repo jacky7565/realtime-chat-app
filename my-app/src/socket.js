b7774926e2e5473e9914c5b800e9678bed6d560e
jacky <jackyyadav7565@gmail.com>
2026-01-03 11:29:11 -0800

    change url

--- my-app/src/socket.js
+++ my-app/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
  let liveServer = "https://realtime-chat-8mc17k0ok-jacky-yadavs-projects.vercel.app/"
+
 // let localServer = "http://localhost:5000"
 export const socket = io(liveServer, {
   withCredentials: true,
